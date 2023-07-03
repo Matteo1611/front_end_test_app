64799cbff07e9de0e79653b68928a865a6bc1e9e
MatteoP <matteo.pulcinelli@iet.it>
2023-07-03 12:49:04 +0200

    aggiunti libri e autori

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,10 @@ import { BibliotecaDataComponent } from './biblioteca-data/biblioteca-data.compo
 import { LibriComponent } from './libri/libri.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AutoriComponent } from './autori/autori.component';
+import { PrestitiComponent } from './prestiti/prestiti.component';
+
 
 @NgModule({
   declarations: [
@@ -16,12 +21,21 @@ import { HttpClientModule } from '@angular/common/http';
     PrimoComponentComponent,
     ListaComponentComponent,
     BibliotecaDataComponent,
-    LibriComponent
+    LibriComponent,
+    HomeComponent,
+    AutoriComponent,
+    PrestitiComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+     RouterModule.forRoot([
+          { path: 'home', component: HomeComponent },
+          { path: 'autori', component: AutoriComponent },
+          { path: 'libri', component: LibriComponent },
+
+        ])
   ],
   providers: [],
   bootstrap: [AppComponent]
